05adb0bd1df0118481db87a67e5ef2a88b221249
carla <118021476+carlanebre@users.noreply.github.com>
2022-11-17 10:03:39 +0100

    Add files via upload

---
+++ tablas-1-10.py
@@ -0,0 +1,5 @@
+# SOLUCIÓN FOR ANIDADO
+
+for i in range(11): #declara las tablas (i toma el valor 1 del rango)
+    for j in range(11): #declara cada multiplicación dentro de cada tabla
+        print(f'{i} x {j} = {i*j}') #print con formato
